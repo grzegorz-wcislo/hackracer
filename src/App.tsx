b4479ea3f2d858653c5a9ef625a02f3b7988fbee
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-05-17 19:36:21 +0200

    Start working on typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App(): JSX.Element {
+  const text = 'Lorem ipsum';
+
+  const [garbage, setGarbage] = useState<string>('');
+  const [timestamps, setTimestamps] = useState<number[]>([]);
+
+  const onKeyDown = (e: KeyboardEvent): void => {
+    const key = e.key;
+    console.log(e);
+
+    const index = timestamps.length;
+
+    if (key === 'Backspace') {
+      setGarbage((g) => g.slice(0, g.length - 1));
+      return;
+    }
+
+    if (key.length > 1) return;
+
+    if (text[index] === key) {
+      setTimestamps([...timestamps, Date.now()]);
+    } else {
+      console.log({ garbage });
+      setGarbage((g) => g + key);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', onKeyDown);
+    return (): void => document.removeEventListener('keydown', onKeyDown);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{text}</p>
+      <p>{garbage}</p>
     </div>
   );
 }
